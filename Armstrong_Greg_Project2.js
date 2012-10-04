287cfa0e9a76643247311f725842e76688219f9d
GREG ARMSTRONG <Greg@GREG-ARMSTRONGs-MacBook-Pro.local>
2012-10-04 14:48:17 -0400

    added procedure

--- Armstrong_Greg_Project2.js
+++ Armstrong_Greg_Project2.js
@@ -12,14 +12,14 @@ console.log ("It has been " + daysSinceOutbreak + " days since the dead started 
 console.log ("I have come across another survivor " + survivors[2] + " while looking for supplies.");
 console.log ("We have been looking for other survivors along with any supplies we can find.");
 
-var watchForZombies = function(name) {
+var watchForZombie =  function(name) {
 	var currentHour = (new Date()).getHours(),
-	timeOfDay;
-	if (currentHour < 5) { timeOfDay = "early morning"; }
+		timeOfDay;
+	if (currentHour < 12) { timeOfDay = "morning"; }
 	else if (currentHour < 17) { timeOfDay = "afternoon"; }
-	else { timeOfDay = "night";}
-	console.log ("Hey it's " + timeOfDay + " keep your eyes open for " + name); 
+	else { timeOfDay = "night"; }
+	console.log("Hey it's " +timeOfDay + ", keep your eyes open for " + name + "!");
 
 };
 
-lookForZombies = ("zombies");
+watchForZombie("zombies");
