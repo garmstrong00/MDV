b52ad7036da2b024d5aa175801d51564c0a302e9
GREG ARMSTRONG <Greg@GREG-ARMSTRONGs-MacBook-Pro.local>
2012-10-04 13:55:57 -0400

    project start

--- Armstrong_Greg_Project2.js
+++ Armstrong_Greg_Project2.js
@@ -1,1 +1,14 @@
-alert("JavaScript works!");
+// Greg Armstrong
+// SDI 1210
+// Project 2
+
+// My Variables
+
+var myName = "Greg";
+var daysSinceOutbreak = 30;
+var survivors = ["Mike", "Sam", "Lisa", "Jake"];
+
+console.log ("My name is " + myName +".");
+console.log ("It has been " + daysSinceOutbreak + " days since the dead started walking");
+console.log ("I have come across another survivor " + survivors[2] + " while looking for supplies.");
+
