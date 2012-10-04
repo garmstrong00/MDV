86922aa9a0e2f3ebfc3a92c7b8c869ba39605ce9
GREG ARMSTRONG <Greg@GREG-ARMSTRONGs-MacBook-Pro.local>
2012-10-04 14:26:28 -0400

    edited HTML file added more JS

--- Armstrong_Greg_Project2.js
+++ Armstrong_Greg_Project2.js
@@ -2,7 +2,6 @@
 // SDI 1210
 // Project 2
 
-// My Variables
 
 var myName = "Greg";
 var daysSinceOutbreak = 30;
@@ -11,4 +10,16 @@ var survivors = ["Mike", "Sam", "Lisa", "Jake"];
 console.log ("My name is " + myName +".");
 console.log ("It has been " + daysSinceOutbreak + " days since the dead started walking");
 console.log ("I have come across another survivor " + survivors[2] + " while looking for supplies.");
+console.log ("We have been looking for other survivors along with any supplies we can find.");
 
+var watchForZombies = function(name) {
+	var currentHour = (new Date()).getHours(),
+	timeOfDay;
+	if (currentHour < 5) { timeOfDay = "early morning"; }
+	else if (currentHour < 17) { timeOfDay = "afternoon"; }
+	else { timeOfDay = "night";}
+	console.log ("Hey it's " + timeOfDay + " keep your eyes open for " + name); 
+
+};
+
+lookForZombies = ("zombies");
