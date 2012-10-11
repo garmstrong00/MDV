604edb3ec5a54f5ebb4a90d4f26436e4ab54f86e
GREG ARMSTRONG <Greg@GREG-ARMSTRONGs-MacBook-Pro.local>
2012-10-11 03:34:03 -0400

    added json data

---
+++ armstrong_greg_project3.js
@@ -0,0 +1,35 @@
+//Greg Armstrong
+//SDI 1210
+//Project 3
+
+//JSON
+
+
+var jsonData = {
+	"survivors" : [
+		{
+			"age": 20,
+			"name": "Max",
+			"job": "Marine",
+			"favoritefood": "Meat"
+		},
+		{
+			"age": 21,
+			"name": "Lisa",
+			"job": "Teacher",
+			"favoritefood": "Sweets"		
+		},
+		{
+			"age": 32,
+			"name": "Jake",
+			"job": "Scientist",
+			"favoritefood": "Fruit"
+		},
+		{
+			"age": 19,
+			"name": "Sam",
+			"job": "Student",
+			"favoritefood": "Vegan"
+		}
+	]
+};
